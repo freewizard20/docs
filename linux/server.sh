1a0c93e85b53ba8733d6f62df528ec02a40b25de
fwcorp <gseongkyu79@gmail.com>
2020-03-30 10:25:48 +0900

    d

--- linux/server.sh
+++ linux/server.sh
@@ -1,11 +1,46 @@
 #!/bin/bash
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gqg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-sudo docker run hello-world
+# node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+nvm install node
+
+# puppeteer
+sudo apt-get install -y gconf-service
+sudo apt-get install -y libasound2
+sudo apt-get install -y libatk1.0-0
+sudo apt-get install -y libatk-bridge2.0-0
+sudo apt-get install -y libc6
+sudo apt-get install -y libcairo2
+sudo apt-get install -y libcups2
+sudo apt-get install -y libdbus-1-3
+sudo apt-get install -y libexpat1
+sudo apt-get install -y libfontconfig1
+sudo apt-get install -y libgcc1
+sudo apt-get install -y libgconf-2-4
+sudo apt-get install -y libgdk-pixbuf2.0-0
+sudo apt-get install -y libglib2.0-0
+sudo apt-get install -y libgtk-3-0
+sudo apt-get install -y libnspr4
+sudo apt-get install -y libpango-1.0-0
+sudo apt-get install -y libpangocairo-1.0-0
+sudo apt-get install -y libstdc++6
+sudo apt-get install -y libx11-6
+sudo apt-get install -y libx11-xcb1
+sudo apt-get install -y libxcb1
+sudo apt-get install -y libxcomposite1
+sudo apt-get install -y libxcursor1
+sudo apt-get install -y libxdamage1
+sudo apt-get install -y libxext6
+sudo apt-get install -y libxfixes3
+sudo apt-get install -y libxi6
+sudo apt-get install -y libxrandr2
+sudo apt-get install -y libxrender1
+sudo apt-get install -y libxss1
+sudo apt-get install -y libxtst6
+sudo apt-get install -y ca-certificates
+sudo apt-get install -y fonts-liberation
+sudo apt-get install -y libappindicator1
+sudo apt-get install -y libnss3
+sudo apt-get install -y lsb-release
+sudo apt-get install -y xdg-utils
+sudo apt-get install -y wget
