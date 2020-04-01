85616ee4a88e1a7fa9f0d98228329ccd13534a3e
fwcorp <gseongkyu79@gmail.com>
2020-04-01 21:00:39 +0900

    ㅇ

---
+++ code/vanilla.js
@@ -0,0 +1,25 @@
+/*
+
+템플릿 리터럴과 Date를 이용한 지연 로직
+
+*/
+let start_time = Date.now();
+while(Date.now()-start_time<3000){
+    let curr_time = Date.now();
+    console.log(`akk from ${start_time}`);
+    while(Date.now() - curr_time < 1000){
+        // sleep
+    }
+}
+
+/*
+
+Delay 함수
+
+*/
+function fw_delay(milliseconds){
+    let current_time = Date.now();
+    while(Date.now() - current_time < milliseconds){
+        // sleep
+    }
+}
