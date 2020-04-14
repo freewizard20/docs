900153476b702a965ce2ec4ed3e4f7a77543cf92
fwcorp <gseongkyu79@gmail.com>
2020-04-14 10:20:36 +0900

    adding new code

---
+++ linux/essentials.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install -y gnome-tweaks htop iftop ffmpeg curl
+sudo apt-get install -y powertop
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+
---
+++ linux/npm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+nvm install 13
+nvm use 13
+npm -g install pm2
+npm -g install ytdl
+
