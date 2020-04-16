951615d33c71920a390c6a85bd8b3ce29f438f12
fwcorp <gseongkyu79@gmail.com>
2020-04-16 09:51:18 +0900

    adding new code

--- linux/lol2.sh
+++ linux/lol2.sh
@@ -4,3 +4,4 @@ sudo snap install wine-platform-5-stable
 snap install leagueoflegends --edge --devmode
 snap refresh --candidate wine-platform-5-staging
 snap refresh --candidate wine-platform-runtime
+sudo snap set system snapshots.automatic.retention=np
