00e49b4caa304569f309c92560bc3b69130f7440
fwcorp <gseongkyu79@gmail.com>
2020-04-16 09:56:29 +0900

    adding new code

--- linux/essentials.sh
+++ linux/essentials.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y gnome-tweaks htop iftop ffmpeg curl
+sudo apt install -y gnome-tweaks htop iftop ffmpeg curl tmux
 sudo apt-get install -y powertop
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
