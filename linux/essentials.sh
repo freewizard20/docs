937725f2d724b7dae7fb7e1979a4597a35118f7c
fwcorp <gseongkyu79@gmail.com>
2020-04-22 20:09:54 +0900

    adding new code

--- linux/essentials.sh
+++ linux/essentials.sh
@@ -3,5 +3,5 @@
 sudo apt install -y gnome-tweaks htop iftop ffmpeg curl tmux
 sudo apt-get install -y powertop
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-
+sudo apt -y install net-tools
 
