810a76c3e081193c53f8239f63ad3d1a1e779a9d
fwcorp <gseongkyu79@gmail.com>
2020-06-29 19:06:53 +0900

    adding new code

---
+++ linux/server/pingtest.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -s -w 'Testing Website Response Time for :%{url_effective}\n\nLookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nAppCon Time:\t\t%{time_appconnect}\nRedirect Time:\t\t%{time_redirect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null https://www.naver.com
